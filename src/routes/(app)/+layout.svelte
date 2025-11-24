6d83f9d5b1a73f2e436c7d257bc32cc7faa31901
killbasa <git@killbasa.com>
2025-11-23 22:12:18 -0500

    move preloading

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { getEvents } from './events/events.remote';
+	import { getProjects } from './projects/projects.remote';
 	import Header from '$components/Header.svelte';
 	import type { LayoutProps } from './$types';
 
 	let { children }: LayoutProps = $props();
+
+	const events = await getEvents();
+	const projects = await getProjects();
 </script>
+
+<svelte:head>
+	<link rel="preload" as="image" href="/images/ProfilePicture.png" />
+
+	{#each events as { image }}
+		<link rel="preload" as="image" href={image} />
+	{/each}
+
+	{#each projects.images as image}
+		<link rel="preload" as="image" href={image} />
+	{/each}
+</svelte:head>
 
 <div class="flex flex-col min-h-screen bg-primary text-color-text">
 	<Header />
